5d7b1206689313ed7d45dfd13e00ac07f58f9f3b
AllanAmaral <allanamaral.25@gmail.com>
2016-05-24 18:42:37 -0300

    Reestruturação de código.

--- src/Negocio/Objetos/HistoricoColeta.java
+++ src/Negocio/Objetos/HistoricoColeta.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Objetos;
+package Negocio.Objetos;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
--- src/Negocio/Objetos/Partida.java
+++ src/Negocio/Objetos/Partida.java
@@ -1,4 +1,4 @@
-package Objetos;
+package Negocio.Objetos;
 
 import java.util.List;
 
