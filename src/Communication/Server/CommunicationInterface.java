59c8df8ccced18c1ec6e2a2fbe85357f243ec06b
AllanAmaral <allanamaral.25@gmail.com>
2016-05-30 16:07:58 -0300

    Adequações

---
+++ src/Communication/Server/CommunicationInterface.java
@@ -0,0 +1,48 @@
+package Communication.Server;
+
+import Busines.Objects.Caminhao;
+import Busines.Objects.Lixeira;
+import Busines.Objects.Motorista;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Allan.Amaral
+ * 
+ * Declara os métodos que serão invocados remotamente
+ */
+public interface CommunicationInterface extends Remote {
+
+    //Inicio Motorista
+    
+    public int registrarMotorista(Motorista motorista) throws RemoteException;
+    
+    public boolean editarMotorista(Motorista motorista) throws RemoteException;
+    
+    public boolean excluirMotorista(Motorista motorista) throws RemoteException;
+    
+    public Motorista buscarMotorista(String nomeMotorista) throws RemoteException;
+
+    //Fim Motorista
+    
+    //Inicio Caminhao
+    
+    public int registraCaminhao(Caminhao caminhao) throws RemoteException;
+    
+    public boolean editarCaminhao(Caminhao caminhao) throws RemoteException;
+    
+    public boolean excluirCaminhao(Caminhao caminhao) throws RemoteException;
+    
+    //Fim Caminhao
+    
+    //Inicio Lixeira
+    
+    public int registraLixeira(Lixeira lixeira) throws RemoteException;
+    
+    public boolean editarLixeira(Lixeira lixeira) throws RemoteException;
+    
+    public boolean excluirLixeira(Lixeira lixeira) throws RemoteException;
+    
+    //Fim Lixeira
+}
