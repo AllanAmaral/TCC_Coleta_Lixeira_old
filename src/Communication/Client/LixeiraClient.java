f3d532e19be5a31e0681393b6978dda50b8f3f80
AllanAmaral <allanamaral.25@gmail.com>
2016-05-30 16:31:28 -0300

    Adequações

---
+++ src/Communication/Client/LixeiraClient.java
@@ -0,0 +1,43 @@
+package Communication.Client;
+
+import Business.FacadeBusiness;
+import java.awt.HeadlessException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import Communication.Server.CommunicationInterface;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Allan.Amaral
+ * 
+ * Responsável pela interação entre usuário e processo­servidor
+ */
+public class LixeiraClient {
+
+    private static FacadeBusiness facade;
+    
+    public static void main(String[] args) throws Exception {
+
+        try {
+            CommunicationInterface communicationInterface = (CommunicationInterface) Naming.lookup("//localhost/TCC_Coleta_Lixo_RMI");
+            facade = new FacadeBusiness();
+            
+            Thread.sleep(5000);
+            
+        } catch (NotBoundException | MalformedURLException | RemoteException | HeadlessException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void enviarStatus(Integer idLixeira, BigDecimal coletadoLixeiraKg, BigDecimal coletadoLixeiraLt) {
+        try {
+            facade.enviarStatus(idLixeira, coletadoLixeiraKg, coletadoLixeiraLt);
+            
+        } catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
